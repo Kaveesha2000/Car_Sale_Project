c6723fcaff7904abc64fc27bf0732468fe03c418
Kaveesha2000 <kaveesha200066@gmail.com>
2022-10-08 08:41:43 +0530

    completing login page

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { View, Text,TextInput,Button,StyleSheet,TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Logo from "./assets/login.jpg"
 
 export default function Login(){
     return(
         <View style = {StyleSheet.container}>
+
+            <Image source={Logo} alt = "Login png" style = {styles.image1}/>
 
             <TextInput style = {styles.input1} placeholder = 'Username' />
             <TextInput style = {styles.input2} placeholder = 'Password' />
@@ -11,26 +14,37 @@ export default function Login(){
             <TouchableOpacity style = {styles.btn}>
                 <Text style = {{color: '#ffff',fontSize:20}}>Login</Text>
             </TouchableOpacity>
+
+            <Text style = {styles.text1}>Create a new account</Text>
             
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+
+    image1:{
+        width:'50%',
+        marginTop: '10%',
+        marginLeft: '25%'
+    },
+
     input1:{
-        marginTop:'40%',
+        marginTop:'20%',
+        marginLeft : '5%',
         borderWidth: 1,
         padding: 10,
-        width:'80%',
+        width:'90%',
         borderRadius:100
 
     },
 
     input2:{
         marginTop:'5%',
+        marginLeft : '5%',
         borderWidth: 1,
         padding: 10,
-        width:'80%',
+        width:'90%',
         borderRadius:100
     },
 
@@ -42,12 +56,20 @@ const styles = StyleSheet.create({
     btn:{
         width:'60%',
         padding:5,
-        backgroundColor:"green",
+        backgroundColor:"blue",
         height:50,
         alignItems:'center',
         justifyContent:'center',
         marginTop:'3%',
+        marginLeft : '20%',
         borderRadius:100
 
+    },
+
+    text1:{
+        color : 'blue',
+        marginTop : '10%',
+        marginLeft : '35%',
+        textDecorationLine : 'underline'
     }
 });
